5d046b5610509208dd5f69bc958d5e454b4beee6
niuliangtao <niuliangtao@qq.com>
2023-10-26 16:29:52 +0800

    add

---
+++ setup.py
@@ -0,0 +1,13 @@
+from funpypi import setup
+
+install_requires = ["funpypi","funfile","requests","tqdm"]
+
+setup(
+    name="funllm",
+    install_requires=install_requires,
+    entry_points={
+        "console_scripts": [
+            "funget = funget.script:funget",
+        ]
+    },
+)
