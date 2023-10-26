7de49d1943949ad8b45093ca84af63159c5e1782
niuliangtao <niuliangtao@qq.com>
2023-10-26 16:38:48 +0800

    add

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from funpypi import setup
 install_requires = ["funpypi","funfile","requests","tqdm"]
 
 setup(
-    name="funtpp",
+    name="funidea",
     install_requires=install_requires,
     entry_points={
         "console_scripts": [
