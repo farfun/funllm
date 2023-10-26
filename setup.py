b2a78b25de71ed473cb335a5bc570f9e33c7d8f1
niuliangtao <niuliangtao@qq.com>
2023-10-26 16:39:14 +0800

    add

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from funpypi import setup
 install_requires = ["funpypi","funfile","requests","tqdm"]
 
 setup(
-    name="funidea",
+    name="funchat",
     install_requires=install_requires,
     entry_points={
         "console_scripts": [
             "funget = funget.script:funget",
         ]
     },
+    version='0.0.1'
 )
