66a9cfb76cb4ada9943f81b9e04d684104aa12a2
niuliangtao <niuliangtao@qq.com>
2023-10-26 16:42:48 +0800

    add

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from funpypi import setup
 install_requires = ["funpypi","funfile","requests","tqdm"]
 
 setup(
-    name="funmeta",
+    name="funweb",
     install_requires=install_requires,
     entry_points={
         "console_scripts": [
